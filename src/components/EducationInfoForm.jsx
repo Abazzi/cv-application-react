a156e3b1e6f81e65528409d251b792833c8cdb0e
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-10-07 11:17:54 -0400

    feat: split return statement and component declaration in EducationInfoForm

--- src/components/EducationInfoForm.jsx
+++ src/components/EducationInfoForm.jsx
@@ -1,44 +1,57 @@
-export default function EducationInfoForm({ education, setEducation }) {
+export default function EducationInfoForm({ education, handleEduInputChange, addEducation }) {
+
+  const educationForm = education.map((edu, index) =>
+    <div key={index} id={edu.id} className="education-form">
+      <h2 className="heading">
+        Education {index + 1}
+      </h2>
+      <div className="school"> <input
+        type="text"
+        placeholder="School"
+        name="school"
+        id="school"
+        maxLength={20}
+        value={edu.school}
+        onChange={(e) => handleEduInputChange(e, edu.id)} />
+        <input
+          type="text"
+          placeholder="Program"
+          name="program"
+          id="program"
+          maxLength={20}
+          value={edu.program}
+          onChange={(e) => handleEduInputChange(e, edu.id)} />
+      </div>
+      <textarea
+        type="text"
+        name="description"
+        id="description"
+        placeholder="Description of program"
+        value={edu.description}
+        onChange={(e) => handleEduInputChange(e, edu.id)} />
+      <div className="dateRange">
+        <input
+          type="text"
+          id="startDate"
+          name="startDate"
+          placeholder="Start Year"
+          value={edu.startDate}
+          onChange={(e) => handleEduInputChange(e, edu.id)} />
+        <input
+          type="text"
+          id="endDate"
+          name="endDate"
+          placeholder="End Year"
+          value={edu.endDate}
+          onChange={(e) => handleEduInputChange(e, edu.id)} />
+      </div>
+    </div >
+  );
 
   return (
     <>
-      <div className="education-form">
-        <h2 className="heading">
-          Education
-        </h2>
-        <div className="school">
-          <input
-            type="text"
-            placeholder="School"
-            maxLength={20}
-            value={education.school}
-            onChange={(event) => setEducation({ ...education, school: event.target.value })} />
-          <input
-            type="text"
-            placeholder="Program"
-            maxLength={20}
-            value={education.program}
-            onChange={(event) => setEducation({ ...education, program: event.target.value })} />
-        </div>
-        <textarea
-          type="text"
-          placeholder="Description"
-          value={education.email}
-          onChange={(event) => setEducation({ ...education, description: event.target.value })} />
-        <div className="dateRange">
-          <input
-            type="text"
-            placeholder="Start Year"
-            value={education.startDate}
-            onChange={(event) => setEducation({ ...education, startDate: event.target.value })} />
-          <input
-            type="text"
-            placeholder="End Year"
-            value={education.endDate}
-            onChange={(event) => setEducation({ ...education, endDate: event.target.value })} />
-        </div>
-      </div>
+      <div>{educationForm}</div>
+      <button className="add-button" onClick={addEducation} >+</button>
     </>
   )
-
 }
